c385ef3dd84897aaebc28828fcc05ed2803a3c78
Brandon Kuczenski <bkuczenski@bren.ucsb.edu>
2015-07-15 12:28:51 -0700

    hide last change with css, switch to KBCALR backend

--- dist/config.js
+++ dist/config.js
@@ -2,6 +2,6 @@
  * Deployment configuration file
  */
 angular.module('lcaApp.config', [])
-    .constant('API_ROOT', "http://publictest.calrecycle.ca.gov/lcatoolapi/api/")
-    //.constant('API_ROOT', "http://kbcalr.isber.ucsb.edu/api/")
+    //.constant('API_ROOT', "http://publictest.calrecycle.ca.gov/lcatoolapi/api/")
+    .constant('API_ROOT', "http://kbcalr.isber.ucsb.edu/api/")
     .constant('HELP_ROOT', "https://github.com/uo-lca/uo-lca.github.io/wiki");
