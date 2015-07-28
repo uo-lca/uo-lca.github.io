24059ec4013416485e2105dd56682b36bd925af5
Sabina Beraha <binaworks@verizon.net>
2015-07-28 11:03:41 -0700

    Test stakeholder feedback deployment

--- dist/config.js
+++ dist/config.js
@@ -2,6 +2,7 @@
  * Deployment configuration file
  */
 angular.module('lcaApp.config', [])
+    //.constant('API_ROOT', "http://localhost:60393/api/")
     //.constant('API_ROOT', "http://publictest.calrecycle.ca.gov/lcatoolapi/api/")
     .constant('API_ROOT', "http://kbcalr.isber.ucsb.edu/api/")
     .constant('HELP_ROOT', "https://github.com/uo-lca/uo-lca.github.io/wiki");
