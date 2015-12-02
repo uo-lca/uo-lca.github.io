98164ce2dc57216f15a5e38d8c37e244d956d85b
bkuczenski <brandon.kuczenski@301south.net>
2015-12-01 22:37:40 -0800

    Transition API hosting to antelope-lca.net

--- dist/config.js
+++ dist/config.js
@@ -4,5 +4,6 @@
 angular.module('lcaApp.config', [])
     //.constant('API_ROOT', "http://localhost:60393/api/")
     //.constant('API_ROOT', "http://publictest.calrecycle.ca.gov/lcatoolapi/api/")
-    .constant('API_ROOT', "http://kbcalr.isber.ucsb.edu/api/")
+    //.constant('API_ROOT', "http://kbcalr.isber.ucsb.edu/api/")
+    .constant('API_ROOT', "http://antelope-lca.net/uo-lca/api/")
     .constant('HELP_ROOT', "https://github.com/uo-lca/uo-lca.github.io/wiki");
